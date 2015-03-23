1246f096f14dffa401cbc536680cbd2618240021
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-03-23 22:04:56 +0900

    Introduce del, gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
+var del = require('del');
 var gulp = require('gulp');
 var riot = require('gulp-riot');
+var watch = require('gulp-watch');
 
-gulp.task('riot', function(){
-  gulp.src('./app/assets/riot-tags/*.tag')
+var compileRiot = function() {
+  gulp.src('./app/assets/riot-tags/**/*.tag')
     .pipe(riot())
     .pipe(gulp.dest('./app/assets/javascripts/riot/'));
-});
+}
 
-gulp.task('watch', function(){
-  gulp.watch('./app/assets/riot-tags/*.tag', ['riot']);
+gulp.task('riot', compileRiot);
+
+gulp.task('watch', function() {
+  watch('./app/assets/riot-tags/**/*.tag', function(file) {
+    if (file.event == 'unlink') {
+      path = './app/assets/javascripts/riot/' +
+        file.path.slice(21).replace(/\.tag$/, '.js')
+      del([path], function(err, paths) {
+        console.log('Deleted files/folders:\n', paths.join('\n'))
+      });
+    }
+    else {
+      compileRiot();
+    }
+  });
 });
 
 gulp.task('default', ['riot']);
